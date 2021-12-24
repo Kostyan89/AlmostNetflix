14733666eff7d3698733f8fc3ac22831e87cb74e
Kostyan89 <kostasamin@gmail.com>
2021-12-24 20:34:23 +0200

    some changes

--- project/views/auth.py
+++ project/views/auth.py
@@ -4,15 +4,16 @@ from flask import request
 from flask_restx import Resource, Namespace, abort
 from marshmallow import ValidationError
 
+from project.dao.models import User
 from project.dao.models.auth import AuthValidator
-from project.implemented import auth_service
+from project.implemented import auth_service, user_schema
 
 
 auth_ns = Namespace('auth')
 
 
-@auth_ns.route('/')
-class AuthView(Resource):
+@auth_ns.route('/login')
+class AuthViewLogin(Resource):
     def post(self):
         try:
             data = AuthValidator().load(request.json)
@@ -30,3 +31,16 @@ class AuthView(Resource):
             abort(400)
         tokens = auth_service.update(request.json)
         return tokens, 201
+
+@auth_ns.route('/register')
+class AuthViewRegister(Resource):
+    def post(self):
+        try:
+            data = user_schema.load(request.json)
+            new_user = User(**data)
+            return new_user, 201
+        except ValidationError:
+            abort(
+                code=HTTPStatus.BAD_REQUEST,
+                message="Не могу создать, чего-то не хватает"
+            )
