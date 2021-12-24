c80576aa8d08d66f5545435f621982b163309a12
Kostyan89 <kostasamin@gmail.com>
2021-12-24 16:48:19 +0200

    add fucn create in user.dao

--- project/dao/user.py
+++ project/dao/user.py
@@ -2,8 +2,10 @@ import base64
 import hashlib
 import hmac
 
+from flask_restx import abort
 from sqlalchemy.orm.scoping import scoped_session
 
+from project.constants import PWD_HASH_SALT, PWD_HASH_ITERATIONS
 from project.dao.models import User
 
 
@@ -14,8 +16,21 @@ class UserDAO:
     def get_by_id(self, pk):
         return self._db_session.query(User).filter(User.id == pk).one_or_none()
 
+    def get_by_email(self, email):
+        return self._db_session.query(User).filter(User.email == email).one_or_none()
+
     def get_all(self):
         return self._db_session.query(User).all()
+
+    def create(self, user_d):
+        ent = User(**user_d)
+        data_for_check = User.get_by_email(user_d.email)
+        if user_d["email"] == data_for_check:
+            abort(405)
+
+        self.session.add(ent)
+        self.session.commit()
+        return ent
 
     def get_hash(password):
         return hashlib.pbkdf2_hmac(
@@ -29,3 +44,4 @@ class UserDAO:
         return hmac.compare_digest(
             base64.b64decode(get_hash),
             hashlib.pbkdf2_hmac('sha256', password.encode(), PWD_HASH_SALT, PWD_HASH_ITERATIONS))
+
